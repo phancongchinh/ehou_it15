2952403d2ee65c0306d0962418ac75a3306319e0
chinhpc <phancongchinhpqa@gmail.com>
2023-12-18 00:19:54 +0700

    Add project files.

---
+++ database-definition.sql
@@ -0,0 +1,10 @@
+﻿create database ehou_it15;
+
+create table app_user (
+	id int identity (1, 1),
+	username varchar(50) not null,
+	password_hash varchar(max) not null,
+	full_name varchar(max) not null,
+	email varchar(max) not null,
+	phone varchar(max) not null
+);
